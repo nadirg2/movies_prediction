9528bbb38a261cddf982d875b3c73efcc9b50f22
nadir <gulievnadir3@gmail.com>
2025-02-08 23:11:23 +0300

    update: airflow

---
+++ app/dags/ml_pipeline.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from app.scripts.data_preprocessing import preprocess_data
+from app.scripts.model_training import train_model
+from app.scripts.model_evaluation import evaluate_model
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'ml_pipeline',
+    default_args=default_args,
+    description='ML pipeline',
+    schedule_interval='@daily',
+)
+
+preprocess_task = PythonOperator(
+    task_id='preprocess_data',
+    python_callable=preprocess_data,
+    dag=dag,
+)
+
+train_task = PythonOperator(
+    task_id='train_model',
+    python_callable=train_model,
+    dag=dag,
+)
+
+evaluate_task = PythonOperator(
+    task_id='evaluate_model',
+    python_callable=evaluate_model,
+    dag=dag,
+)
+
+preprocess_task >> train_task >> evaluate_task
